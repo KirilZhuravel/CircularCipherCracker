d23796c57563aec88d3cc302fec34409b85923cc
Bormoley <kirilzhuravel8@gmail.com>
2026-02-02 18:17:38 +0200

    Add TextUtils for string sanitization and frequency analysis
      Implemented clean() method to filter non-alphabetic characters.
    
      Added getFrequencies() to calculate character distribution percentages.
    
       Prepared utility class for heuristic analysis.

--- src/main/java/cipher/utils/TextUtils.java
+++ src/main/java/cipher/utils/TextUtils.java
@@ -1,4 +1,27 @@
 package cipher.utils;
 
 public class TextUtils {
+
+    public static String clean(String input) {
+        if (input == null) return "";
+        return input.toUpperCase().replaceAll("[^A-Z]", "");
+    }
+
+    public static double[] getFrequencies(String text) {
+        double[] frequencies = new double[26];
+        String cleanText = clean(text);
+        int length = cleanText.length();
+
+        if (length == 0) return frequencies;
+
+        for (char c : cleanText.toCharArray()) {
+            frequencies[c - 'A']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            frequencies[i] = (frequencies[i] / length) * 100.0;
+        }
+
+        return frequencies;
+    }
 }
