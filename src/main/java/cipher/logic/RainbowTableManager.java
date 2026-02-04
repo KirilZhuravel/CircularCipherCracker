f322c076c7197cf409fd337ae5406b3971d409aa
Bormoley <kirilzhuravel8@gmail.com>
2026-02-04 20:22:27 +0200

    Create RainbowTableManager skeleton
    - Initialized the main logic class for Rainbow Tables.
    - Added internal HashMap storage for chain endpoints.
    - Defined constant path for table storage.

--- src/main/java/cipher/logic/RainbowTableManager.java
+++ src/main/java/cipher/logic/RainbowTableManager.java
@@ -1,4 +1,29 @@
 package cipher.logic;
 
+import cipher.model.Candidate;
+import cipher.utils.FileIO;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RainbowTableManager {
+
+    private static final String TABLE_FILE = "data/rainbow_table.dat";
+
+
+    private Map<String, String> rainbowTable;
+
+    public RainbowTableManager() {
+        this.rainbowTable = new HashMap<>();
+    }
+
+
+    public void clearTable() {
+        this.rainbowTable.clear();
+        System.out.println("Rainbow Table memory cleared.");
+    }
+
+    public int getSize() {
+        return rainbowTable.size();
+    }
 }
