a1b4673e28dcda6eddf039466355ac34b3836e8d
Bormoley <kirilzhuravel8@gmail.com>
2026-02-04 20:12:47 +0200

    Implement TrieNode model for dictionary lookups
    - Added serializable TrieNode class.
    - Implemented node structure using HashMap for child tracking.
    - Prepared data structure for high-performance dictionary validation.

--- src/main/java/cipher/model/TrieNode.java
+++ src/main/java/cipher/model/TrieNode.java
@@ -1,4 +1,23 @@
 package cipher.model;
 
-public class TrieNode {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TrieNode implements Serializable {
+    private final Map<Character, TrieNode> children = new HashMap<>();
+    private boolean isEndOfWord;
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        isEndOfWord = endOfWord;
+    }
 }
